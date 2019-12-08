e6b283d99852fa80366b4f125efd656b1c34f41a
FuyangShan <40450679+FuyangShan@users.noreply.github.com>
2019-12-07 16:02:02 -0800

    Create items based on geolocation

--- WebContent/scripts/main.js
+++ WebContent/scripts/main.js
@@ -13,11 +13,172 @@
    */
   function init() {
     // register event listeners
+    document.querySelector('#login-btn').addEventListener('click', login);
     document.querySelector('#nearby-btn').addEventListener('click', loadNearbyItems);
     document.querySelector('#fav-btn').addEventListener('click', loadFavoriteItems);
     document.querySelector('#recommend-btn').addEventListener('click', loadRecommendedItems);
+    // validateSession();
+    onSessionValid({"user_id":"1111","name":"John Smith","status":"OK"});
+  }
+
+  /**
+   * Session
+   */
+  function validateSession() {
+    onSessionInvalid();
+    // The request parameters
+    var url = './login';
+    var req = JSON.stringify({});
+
+    // display loading message
+    showLoadingMessage('Validating session...');
+
+    // make AJAX call
+    ajax('GET', url, req,
+      // session is still valid
+      function(res) {
+        var result = JSON.parse(res);
+
+        if (result.status === 'OK') {
+          onSessionValid(result);
+        }
+      });
+  }
+
+  function onSessionValid(result) {
+    user_id = result.user_id;
+    user_fullname = result.name;
+
+    var loginForm = document.querySelector('#login-form');
+    var itemNav = document.querySelector('#item-nav');
+    var itemList = document.querySelector('#item-list');
+    var avatar = document.querySelector('#avatar');
+    var welcomeMsg = document.querySelector('#welcome-msg');
+    var logoutBtn = document.querySelector('#logout-link');
+
+    welcomeMsg.innerHTML = 'Welcome, ' + user_fullname;
+
+    showElement(itemNav);
+    showElement(itemList);
+    showElement(avatar);
+    showElement(welcomeMsg);
+    showElement(logoutBtn, 'inline-block');
+    hideElement(loginForm);
+
+    initGeoLocation();
+  }
+
+  function onSessionInvalid() {
+    var loginForm = document.querySelector('#login-form');
+    var itemNav = document.querySelector('#item-nav');
+    var itemList = document.querySelector('#item-list');
+    var avatar = document.querySelector('#avatar');
+    var welcomeMsg = document.querySelector('#welcome-msg');
+    var logoutBtn = document.querySelector('#logout-link');
+
+    hideElement(itemNav);
+    hideElement(itemList);
+    hideElement(avatar);
+    hideElement(logoutBtn);
+    hideElement(welcomeMsg);
+
+    showElement(loginForm);
+  }
+
+  function hideElement(element) {
+    element.style.display = 'none';
+  }
+
+  function showElement(element, style) {
+    var displayStyle = style ? style : 'block';
+    element.style.display = displayStyle;
+  }
+
+  function initGeoLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        onPositionUpdated,
+        onLoadPositionFailed, {
+          maximumAge: 60000
+        });
+      showLoadingMessage('Retrieving your location...');
+    } else {
+      onLoadPositionFailed();
+    }
+  }
+
+  function onPositionUpdated(position) {
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+
     loadNearbyItems();
   }
+
+  function onLoadPositionFailed() {
+    console.warn('navigator.geolocation is not available');
+    getLocationFromIP();
+  }
+
+  function getLocationFromIP() {
+    // get location from http://ipinfo.io/json
+    var url = 'http://ipinfo.io/json'
+    var data = null;
+
+    ajax('GET', url, data, function(res) {
+      var result = JSON.parse(res);
+      if ('loc' in result) {
+        var loc = result.loc.split(',');
+        lat = loc[0];
+        lng = loc[1];
+      } else {
+        console.warn('Getting location by IP failed.');
+      }
+      loadNearbyItems();
+    });
+  }
+
+  // -----------------------------------
+  // Login
+  // -----------------------------------
+
+  function login() {
+    var username = document.querySelector('#username').value;
+    var password = document.querySelector('#password').value;
+    password = md5(username + md5(password));
+
+    // The request parameters
+    var url = './login';
+    var req = JSON.stringify({
+      user_id : username,
+      password : password,
+    });
+
+    ajax('POST', url, req,
+      // successful callback
+      function(res) {
+        var result = JSON.parse(res);
+
+        // successfully logged in
+        if (result.status === 'OK') {
+          onSessionValid(result);
+        }
+      },
+
+      // error
+      function() {
+        showLoginError();
+      },
+      true);
+  }
+
+  function showLoginError() {
+    document.querySelector('#login-error').innerHTML = 'Invalid username or password';
+  }
+
+  function clearLoginError() {
+    document.querySelector('#login-error').innerHTML = '';
+  }
+
 
   // -----------------------------------
   // Helper Functions
@@ -75,22 +236,171 @@
     return element;
   }
 
+  /**
+   * AJAX helper
+   *
+   * @param method - GET|POST|PUT|DELETE
+   * @param url - API end point
+   * @param data - request payload data
+   * @param successCallback - Successful callback function
+   * @param errorCallback - Error callback function
+   */
+  function ajax(method, url, data, successCallback, errorCallback) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open(method, url, true);
+
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        successCallback(xhr.responseText);
+      } else {
+        errorCallback();
+      }
+    };
+
+    xhr.onerror = function() {
+      console.error("The request couldn't be completed.");
+      errorCallback();
+    };
+
+    if (data === null) {
+      xhr.send();
+    } else {
+      xhr.setRequestHeader("Content-Type",
+        "application/json;charset=utf-8");
+      xhr.send(data);
+    }
+  }
+
+  // -------------------------------------
+  // AJAX call server-side APIs
+  // -------------------------------------
+
+  /**
+   * API #1 Load the nearby items API end point: [GET]
+   * /search?user_id=1111&lat=37.38&lon=-122.08
+   */
   function loadNearbyItems() {
-  	activeBtn('nearby-btn');
-  	var nearbyItems = mockSearchResponse;
-  	listItems(nearbyItems);
+    console.log('loadNearbyItems');
+    activeBtn('nearby-btn');
+
+    // The request parameters
+    var url = './search';
+    var params = 'user_id=' + user_id + '&lat=' + lat + '&lon=' + lng;
+    var data = null;
+
+    // display loading message
+    showLoadingMessage('Loading nearby items...');
+
+    // make AJAX call
+    ajax('GET', url + '?' + params, data,
+      // successful callback
+      function(res) {
+        var items = JSON.parse(res);
+        if (!items || items.length === 0) {
+          showWarningMessage('No nearby item.');
+        } else {
+          listItems(items);
+        }
+      },
+      // failed callback
+      function() {
+        showErrorMessage('Cannot load nearby items.');
+      }
+    );
   }
-  
+
+  /**
+   * API #2 Load favorite (or visited) items API end point: [GET]
+   * /history?user_id=1111
+   */
   function loadFavoriteItems() {
-  	activeBtn('fav-btn');
-  	var favoriteItems = mockSearchResponse.slice(3, 6);
-  	listItems(favoriteItems);
+    activeBtn('fav-btn');
+
+    // request parameters
+    var url = './history';
+    var params = 'user_id=' + user_id;
+    var req = JSON.stringify({});
+
+    // display loading message
+    showLoadingMessage('Loading favorite items...');
+
+    // make AJAX call
+    ajax('GET', url + '?' + params, req, function(res) {
+      var items = JSON.parse(res);
+      if (!items || items.length === 0) {
+        showWarningMessage('No favorite item.');
+      } else {
+        listItems(items);
+      }
+    }, function() {
+      showErrorMessage('Cannot load favorite items.');
+    });
   }
-  
+
+  /**
+   * API #3 Load recommended items API end point: [GET]
+   * /recommendation?user_id=1111
+   */
   function loadRecommendedItems() {
-  	activeBtn('recommend-btn');
-  	var recommendedItems = mockSearchResponse.slice(10);
-  	listItems(recommendedItems);
+    activeBtn('recommend-btn');
+
+    // request parameters
+    var url = './recommendation' + '?' + 'user_id=' + user_id + '&lat=' + lat + '&lon=' + lng;
+    var data = null;
+
+    // display loading message
+    showLoadingMessage('Loading recommended items...');
+
+    // make AJAX call
+    ajax('GET', url, data,
+      // successful callback
+      function(res) {
+        var items = JSON.parse(res);
+        if (!items || items.length === 0) {
+          showWarningMessage('No recommended item. Make sure you have favorites.');
+        } else {
+          listItems(items);
+        }
+      },
+      // failed callback
+      function() {
+        showErrorMessage('Cannot load recommended items.');
+      }
+    );
+  }
+
+  /**
+   * API #4 Toggle favorite (or visited) items
+   *
+   * @param item_id - The item business id
+   *
+   * API end point: [POST]/[DELETE] /history request json data: {
+   * user_id: 1111, visited: [a_list_of_business_ids] }
+   */
+  function changeFavoriteItem(item_id) {
+    // check whether this item has been visited or not
+    var li = document.querySelector('#item-' + item_id);
+    var favIcon = document.querySelector('#fav-icon-' + item_id);
+    var favorite = !(li.dataset.favorite === 'true');
+
+    // request parameters
+    var url = './history';
+    var req = JSON.stringify({
+      user_id: user_id,
+      favorite: [item_id]
+    });
+    var method = favorite ? 'POST' : 'DELETE';
+
+    ajax(method, url, req,
+      // successful callback
+      function(res) {
+        var result = JSON.parse(res);
+        if (result.status === 'OK' || result.result === 'SUCCESS') {
+          li.dataset.favorite = favorite;
+          favIcon.className = favorite ? 'fa fa-heart' : 'fa fa-heart-o';
+        }
+      });
   }
 
   // -------------------------------------
